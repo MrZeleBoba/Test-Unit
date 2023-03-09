6cf1880d690d9e3e5a1313d39a41e0fcbb7bfcf7
Aleksey Galishchev <galishchev@inbox.ru>
2023-03-09 21:58:44 +0300

    Test add

--- Author/src/test/java/org/example/AuthorTest.java
+++ Author/src/test/java/org/example/AuthorTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthorTest {
-    private Books books;
+
     private Books actual;
 
 
--- Main/src/main/java/org/example/Main.java
+++ Main/src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        //Author author = new Author("Лев", "Толстой", 0);
-        //Books books = new Books("Война и мир", author, LocalDate.now().getYear(), " ");
-
+//        Author author = new Author("Лев", "Толстой", 0);
+//        Books books = new Books("Война и мир", author, LocalDate.now().getYear(), " ");
+//
 //        System.out.println(books);
 //        books.up();
 //        books.up();
@@ -17,6 +17,6 @@ public class Main {
 //        books.down();
 //        books.down();
 //        books.append("Как давно это было");
-        //System.out.println(books);
+//        System.out.println(books);
     }
 }
